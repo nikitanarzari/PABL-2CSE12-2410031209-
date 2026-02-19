375f94bcf82072e6017da3b1343ca499c4944377
Nikita Narzari <nikitanarzari5@gmail.com>
2026-02-19 11:37:41 +0530

    Renamed and added all exp

---
+++ List-1/Experiment-09.py
@@ -0,0 +1,12 @@
+def two_sum(nums, target):
+    seen = {}  
+
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if complement in seen:
+            return [seen[complement], i]
+        seen[nums[i]] = i
+
+nums = [2, 7, 11, 15]
+target = 9
+print(two_sum(nums, target))
