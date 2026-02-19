edc9d0112e3ee07a07c433e39e10b133902845c5
Nikita Narzari <nikitanarzari5@gmail.com>
2026-02-19 11:08:38 +0530

    updated code

--- Experiment-1.py
+++ Experiment-1.py
@@ -1,5 +1,11 @@
-def reverseArray(arr):
-    l, r = 0, len(arr)-1
-    while l < r:
-        arr[l], arr[r] = arr[r], arr[l]
-        l += 1; r -= 1
+def reverse_array(arr):
+    start, end = 0, len(arr) - 1
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr
+
+
+arr = [1, 4, 3, 2, 6, 5]
+print(reverse_array(arr))
